d31306626d556c71aac22260f37edb32d915c846
Trammell hudson <hudson@trmm.net>
2018-04-07 14:47:46 -0400

    skeleton for p5.js sketch

---
+++ sketch.js
@@ -0,0 +1,19 @@
+/*
+ * Serve this directory with:
+ * python -mSimpleHTTPServer 8000
+ */
+function setup()
+{
+	createCanvas(windowWidth-10, windowHeight-10);
+	//createCanvas(windowWidth-10, windowHeight-10, WEBGL);
+	background(0);
+}
+
+//function keyReleased() { }
+//function keyPressed() { }
+//function mousePressed() { }
+
+function draw()
+{
+	background(0);
+}
